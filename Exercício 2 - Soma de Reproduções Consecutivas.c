9cb654512cd02f6f0c72070ac6d52a63b37de9bf
alexfertos <128864884+alexfertos@users.noreply.github.com>
2026-04-08 22:18:47 -0300

    Exercício - Soma de Reproduções Consecutivas.c
    Exercício: Análise de Popularidade de Músicas
    com Soma de Reproduções Consecutivas

---
+++ Exercício 2 - Soma de Reproduções Consecutivas.c
@@ -0,0 +1,20 @@
+// Exercício 2 - Soma de Reproduções Consecutivas
+#include <stdio.h>
+
+int main() {
+    int reproducoes[] = {50, 90, 120, 30, 200};
+    int tamanho = 5;
+    int maiorSoma = 0;
+
+    for (int i = 0; i < tamanho - 1; i++) {
+        int soma = reproducoes[i] + reproducoes[i + 1];
+
+        if (soma > maiorSoma) {
+            maiorSoma = soma;
+        }
+    }
+
+    printf("Maior soma consecutiva: %d\n", maiorSoma);
+
+    return 0;
+}
