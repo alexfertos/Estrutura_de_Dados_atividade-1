49c785e701dc5a29bd8cc89ae0cc6af6981eefc5
alexfertos <128864884+alexfertos@users.noreply.github.com>
2026-04-08 22:23:44 -0300

    Create Exercício - Vogais e Consoantes.c
    Exercício – Contagem de vogais e consoantes

---
+++ Exercício - Vogais e Consoantes.c
@@ -0,0 +1,26 @@
+// Exercício - Vogais e Consoantes
+#include <stdio.h>
+
+int main() {
+    char s[50];
+    int vogais = 0, consoantes = 0;
+
+    printf("Digite uma string: ");
+    fgets(s, sizeof(s), stdin);
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        char c = s[i];
+
+        if (c >= 'a' && c <= 'z') {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                vogais++;
+            } else {
+                consoantes++;
+            }
+        }
+    }
+
+    printf("%d %d\n", vogais, consoantes);
+
+    return 0;
+}
